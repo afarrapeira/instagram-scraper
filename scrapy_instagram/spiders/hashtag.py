c5ea3b6ec2e548b0515b493b6339686fa7fcbfbd
Angel Farrapeira <afarrapeira@wirelessgalicia.com>
2019-08-29 15:07:30 +0200

    Removed loc_lat and loc_lon properties, fixed loc_id and loc_name

--- scrapy_instagram/spiders/hashtag.py
+++ scrapy_instagram/spiders/hashtag.py
@@ -70,45 +70,35 @@ class InstagramSpider(scrapy.Spider):
     def parse_post(self, response):
         graphql = json.loads(response.text)
         media = graphql['graphql']['shortcode_media']
-        location = media.get('location', {})
-        if location is not None:
-            loc_id = location.get('id', 0)
-            request = scrapy.Request("https://www.instagram.com/explore/locations/"+loc_id+"/?__a=1", callback=self.parse_post_location, dont_filter=True)
-            request.meta['media'] = media
-            yield request
-        else:
-            media['location'] = {}
-            yield self.makePost(media)         
-
-    def parse_post_location(self, response):
-        media = response.meta['media']
-        location = json.loads(response.text)
-        location = location['location']
-        media['location'] = location
-        yield self.makePost(media)
+        yield self.makePost(media)         
 
     def makePost(self, media):
         location = media['location']
         caption = ''
-        comments = 0
-        likes = 0
+        comment = 0
+        like = 0
+        loc_id = 0
+        loc_name = ''
+        if location is not None:
+            loc_id = location['id']
+            loc_name = location['name']
         if len(media['edge_media_to_caption']['edges']):
             caption = media['edge_media_to_caption']['edges'][0]['node']['text']
-        if len(media['edge_media_preview_comment']):
-            comments = media['edge_media_preview_comment']['count']
-        if len(media['edge_media_preview_like']):
-            likes = media['edge_media_preview_like']['count']
+        try:
+            comment = media['edge_media_preview_comment']['count']
+            like = media['edge_media_preview_like']['count']
+        except NameError:
+            pass
+            
         return Post(id=media['id'],
                     shortcode=media['shortcode'],
                     caption=caption,
                     display_url=media['display_url'],
-                    loc_id=location.get('id', 0),
-                    loc_name=location.get('name',''),
-                    loc_lat=location.get('lat',0),
-                    loc_lon=location.get('lng',0),
+                    loc_id=loc_id,
+                    loc_name=loc_name,
                     owner_id =media['owner']['id'],
                     owner_name = media['owner']['username'],
                     taken_at_timestamp= media['taken_at_timestamp'],
-                    comments = comments,
-                    likes = likes
+                    comments= comment,
+                    likes= like
                     )
